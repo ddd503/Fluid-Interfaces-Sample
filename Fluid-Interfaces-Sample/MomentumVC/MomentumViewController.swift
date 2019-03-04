dbe1e5553d3562dd8ca4b86c3e6edb962cbfc9e4
ddd503 <skbsporta7@gmail.com>
2019-03-04 22:43:41 +0900

    add MomentumViewController

---
+++ Fluid-Interfaces-Sample/MomentumVC/MomentumViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MomentumViewController.swift
+//  Fluid-Interfaces-Sample
+//
+//  Created by kawaharadai on 2019/03/04.
+//  Copyright © 2019 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+final class MomentumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
