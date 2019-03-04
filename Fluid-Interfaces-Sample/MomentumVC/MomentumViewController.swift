51d69fbaa91d1d176adc8802be97b72ac22beb5c
ddd503 <skbsporta7@gmail.com>
2019-03-05 00:21:25 +0900

    add cardView for MomentumVC

--- Fluid-Interfaces-Sample/MomentumVC/MomentumViewController.swift
+++ Fluid-Interfaces-Sample/MomentumVC/MomentumViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 final class MomentumViewController: UIViewController {
 
+    private lazy var cardView: UIView = {
+        let view = UIView()
+        view.frame.size = CGSize(width: self.view.bounds.width * 0.8, height: self.view.bounds.height * 0.8)
+        view.center = self.view.center
+        view.backgroundColor = .blue
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.addSubview(cardView)
     }
 
 }
