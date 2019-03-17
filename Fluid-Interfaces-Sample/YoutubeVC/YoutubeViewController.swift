37ecaed6c69229913a8607d1f0b6d2a8614f75e6
ddd503 <skbsporta7@gmail.com>
2019-03-17 13:45:52 +0900

    add pan gesture

--- Fluid-Interfaces-Sample/YoutubeVC/YoutubeViewController.swift
+++ Fluid-Interfaces-Sample/YoutubeVC/YoutubeViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 final class YoutubeViewController: UIViewController {
 
     @IBOutlet private weak var baseView: UIView!
+    @IBOutlet private weak var infomationView: UIView!
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var tableView: UITableView! {
         didSet {
@@ -22,7 +23,31 @@ final class YoutubeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+
+    private func setup() {
         title = interfaceType?.displayName
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(panned(gesture:)))
+        imageView.addGestureRecognizer(panGesture)
+    }
+
+    @objc private func panned(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .changed:
+            let transition = gesture.translation(in: imageView)
+            let diffarence = baseView.bounds.size.height / (baseView.bounds.size.height + transition.y)
+            let scaleTransform = CGAffineTransform(scaleX: 1.0, y: diffarence)
+            let imageViewTranslationTransform = CGAffineTransform(translationX: 0, y: transition.y * 1.2)
+            let infoViewTranslationTransform = CGAffineTransform(translationX: 0, y: transition.y)
+            // 移動と伸縮を同時にやる
+            imageView.transform = scaleTransform.concatenating(imageViewTranslationTransform)
+            infomationView.transform = infoViewTranslationTransform
+        case .ended:
+            print("end")
+        default: break
+        }
     }
 
 }
